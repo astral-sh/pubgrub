b2d4443206a338b4918a4e92045b0c4a66c45467
Zanie <contact@zanie.dev>
2023-11-10 11:18:48 -0600

    Add example for direct conflict

---
+++ examples/unsat_direct_dependency_conflict.rs
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MPL-2.0
+
+use pubgrub::error::PubGrubError;
+use pubgrub::range::Range;
+use pubgrub::report::{DefaultStringReporter, Reporter};
+use pubgrub::solver::{resolve, OfflineDependencyProvider};
+use pubgrub::version::SemanticVersion;
+
+fn main() {
+    let mut dependency_provider = OfflineDependencyProvider::<&str, SemanticVersion>::new();
+    // Define the root package with incompatible versions for a single dependency
+    dependency_provider.add_dependencies(
+        "root",
+        (0, 0, 0),
+        vec![
+            ("foo", Range::exact((1, 0, 0))),
+            ("foo", Range::exact((2, 0, 0))),
+        ],
+    );
+
+    // Run the algorithm
+    match resolve(&dependency_provider, "root", (0, 0, 0)) {
+        Ok(sol) => println!("{:?}", sol),
+        Err(PubGrubError::NoSolution(mut derivation_tree)) => {
+            derivation_tree.collapse_no_versions();
+            eprintln!("{}", DefaultStringReporter::report(&derivation_tree));
+            std::process::exit(1);
+        }
+        Err(err) => panic!("{:?}", err),
+    };
+}
