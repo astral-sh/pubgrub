26dd4fde675586a04446cc87341fc312a7997481
Zanie <contact@zanie.dev>
2023-11-10 11:31:40 -0600

    Add example for root message

---
+++ examples/unsat_root_messages.rs
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MPL-2.0
+
+use pubgrub::error::PubGrubError;
+use pubgrub::range::Range;
+use pubgrub::report::{DefaultStringReporter, Reporter};
+use pubgrub::solver::{resolve, OfflineDependencyProvider};
+use pubgrub::version::SemanticVersion;
+
+fn main() {
+    let mut dependency_provider = OfflineDependencyProvider::<&str, SemanticVersion>::new();
+    // Define the root package with an incompatibility so we get an unsat message
+    // The incompatibility itself doesn't matter
+    dependency_provider.add_dependencies(
+        "root",
+        (0, 0, 0),
+        vec![
+            ("foo", Range::exact((1, 0, 0))),
+            ("foo", Range::exact((2, 0, 0))),
+        ],
+    );
+
+    // Run the algorithm
+    match resolve(&dependency_provider, "root", (0, 0, 0)) {
+        Ok(sol) => println!("{:?}", sol),
+        Err(PubGrubError::NoSolution(mut derivation_tree)) => {
+            eprintln!("No solution.\n");
+
+            eprintln!("### Default report:");
+            eprintln!("```");
+            eprintln!("{}", DefaultStringReporter::report(&derivation_tree));
+            eprintln!("```\n");
+
+            derivation_tree.collapse_no_versions();
+            eprintln!("### Report with `collapse_no_versions`:");
+            eprintln!("```");
+            eprintln!("{}", DefaultStringReporter::report(&derivation_tree));
+            eprintln!("```");
+            std::process::exit(1);
+        }
+        Err(err) => panic!("{:?}", err),
+    };
+}
