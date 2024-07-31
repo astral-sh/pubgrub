b72b7266e8079dac13a5d66bcf313828af868e4f
konstin <konstin@mailbox.org>
2024-07-31 14:26:05 +0200

    Expose internal interfaces used by uv
    This wrapper avoids accessing the `incompatibility_store` directly in uv
    code.
    
    Before:
    
    ```rust
    let dep_incompats = self.pubgrub.add_version(
        package.clone(),
        version.clone(),
        dependencies,
    );
    self.pubgrub.partial_solution.add_version(
        package.clone(),
        version.clone(),
        dep_incompats,
        &self.pubgrub.incompatibility_store,
    );
    ```
    
    After:
    
    ```rust
    self.pubgrub.add_incompatibility_from_dependencies(package.clone(), version.clone(), dependencies);
    ```
    
    `add_incompatibility_from_dependencies` is one of the main methods for
    the custom interface to pubgrub.

--- src/internal/incompatibility.rs
+++ src/internal/incompatibility.rs
@@ -55,9 +55,10 @@ impl ContradictionCache {
 /// during conflict resolution. More about all this in
 /// [PubGrub documentation](https://github.com/dart-lang/pub/blob/master/doc/solver.md#incompatibility).
 #[derive(Debug, Clone)]
-pub(crate) struct Incompatibility<P: Package, VS: VersionSet, M: Eq + Clone + Debug + Display> {
+pub struct Incompatibility<P: Package, VS: VersionSet, M: Eq + Clone + Debug + Display> {
     package_terms: SmallMap<Id<P>, Term<VS>>,
-    kind: Kind<P, VS, M>,
+    /// The reason for the incompatibility.
+    pub kind: Kind<P, VS, M>,
     contradiction_cache: ContradictionCache,
 }
 
@@ -70,8 +71,9 @@ pub(crate) type IncompDpId<DP> = IncompId<
     <DP as DependencyProvider>::M,
 >;
 
+/// The reason for the incompatibility.
 #[derive(Debug, Clone)]
-enum Kind<P: Package, VS: VersionSet, M: Eq + Clone + Debug + Display> {
+pub enum Kind<P: Package, VS: VersionSet, M: Eq + Clone + Debug + Display> {
     /// Initial incompatibility aiming at picking the root package for the first decision.
     ///
     /// This incompatibility drives the resolution, it requires that we pick the (virtual) root
@@ -151,7 +153,7 @@ impl<P: Package, VS: VersionSet, M: Eq + Clone + Debug + Display> Incompatibilit
     }
 
     /// Build an incompatibility from a given dependency.
-    pub(crate) fn from_dependency(package: Id<P>, versions: VS, dep: (Id<P>, VS)) -> Self {
+    pub fn from_dependency(package: Id<P>, versions: VS, dep: (Id<P>, VS)) -> Self {
         let (p2, set2) = dep;
         Self {
             package_terms: if set2 == VS::empty() {
@@ -425,6 +427,7 @@ impl<'a, P: Package, VS: VersionSet + 'a, M: Eq + Clone + Debug + Display + 'a>
 }
 
 impl<P: Package, VS: VersionSet, M: Eq + Clone + Debug + Display> Incompatibility<P, VS, M> {
+    /// Display the incompatibility.
     pub fn display<'a>(&'a self, package_store: &'a HashArena<P>) -> impl Display + 'a {
         match self.iter().collect::<Vec<_>>().as_slice() {
             [] => "version solving failed".into(),
