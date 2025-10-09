40de36e7081c9b9fe2f8ea96cf49c60aabebb708
konstin <konstin@mailbox.org>
2025-10-09 10:29:59 +0200

    Add failing cross platform test for #373
    See https://github.com/pubgrub-rs/pubgrub/issues/373#issuecomment-3384608891.
    
    This test fails on 32-bit, where the iteration order of an `FxHashMap` is different:
    
    ```
    $ cargo test -p pubgrub --test tests same_result_across_platforms --target i686-unknown-linux-gnu -q
    
    running 1 test
    same_result_across_platforms --- FAILED
    
    failures:
    
    ---- same_result_across_platforms stdout ----
    
    thread 'same_result_across_platforms' panicked at tests/tests.rs:137:5:
    assertion `left == right` failed
      left: "964"
     right: "712"
    note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
    
    failures:
        same_result_across_platforms
    
    test result: FAILED. 0 passed; 1 failed; 0 ignored; 0 measured; 3 filtered out; finished in 0.29s
    
    error: test failed, to rerun pass `-p pubgrub --test tests`
    ```

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: MPL-2.0
 
-use pubgrub::{OfflineDependencyProvider, PubGrubError, Ranges, resolve};
+use pubgrub::{
+    Dependencies, DependencyProvider, OfflineDependencyProvider, Package,
+    PackageResolutionStatistics, PubGrubError, Ranges, VersionSet, resolve,
+};
+use std::convert::Infallible;
 
 type NumVS = Ranges<u32>;
 
@@ -49,4 +53,86 @@ fn depend_on_self() {
     assert!(resolve(&dependency_provider, "a", 0u32).is_ok());
     dependency_provider.add_dependencies("a", 66u32, [("a", Ranges::singleton(111u32))]);
     assert!(resolve(&dependency_provider, "a", 66u32).is_err());
+}
+
+/// Test the prioritization is stable across platforms.
+///
+/// https://github.com/pubgrub-rs/pubgrub/issues/373#issuecomment-3384608891
+#[test]
+fn same_result_across_platforms() {
+    struct UnprioritizingDependencyProvider<P: Package, VS: VersionSet> {
+        dependency_provider: OfflineDependencyProvider<P, VS>,
+    }
+
+    impl<P: Package, VS: VersionSet> UnprioritizingDependencyProvider<P, VS> {
+        fn new() -> Self {
+            Self {
+                dependency_provider: OfflineDependencyProvider::new(),
+            }
+        }
+
+        pub fn add_dependencies<I: IntoIterator<Item = (P, VS)>>(
+            &mut self,
+            package: P,
+            version: impl Into<VS::V>,
+            dependencies: I,
+        ) {
+            self.dependency_provider
+                .add_dependencies(package, version, dependencies);
+        }
+    }
+
+    impl<P: Package, VS: VersionSet> DependencyProvider for UnprioritizingDependencyProvider<P, VS> {
+        type P = P;
+        type V = VS::V;
+        type VS = VS;
+        type M = String;
+        type Priority = u32;
+        type Err = Infallible;
+
+        fn choose_version(&self, package: &P, range: &VS) -> Result<Option<VS::V>, Infallible> {
+            self.dependency_provider.choose_version(package, range)
+        }
+
+        fn prioritize(
+            &self,
+            _package: &Self::P,
+            _range: &Self::VS,
+            _package_statistics: &PackageResolutionStatistics,
+        ) -> Self::Priority {
+            0
+        }
+
+        fn get_dependencies(
+            &self,
+            package: &P,
+            version: &VS::V,
+        ) -> Result<Dependencies<P, VS, Self::M>, Infallible> {
+            self.dependency_provider.get_dependencies(package, version)
+        }
+    }
+
+    let mut dependency_provider = UnprioritizingDependencyProvider::<_, NumVS>::new();
+
+    let x = (0..1000)
+        .into_iter()
+        .map(|i| (i.to_string(), Ranges::full()))
+        .collect::<Vec<_>>();
+    dependency_provider.add_dependencies("root".to_string(), 1u32, x);
+
+    for i in 0..1000 {
+        let x = (0..1000)
+            .into_iter()
+            .filter(|j| *j != i)
+            .map(|i| (i.to_string(), Ranges::<u32>::singleton(1u32)))
+            .collect::<Vec<_>>();
+        dependency_provider.add_dependencies(i.to_string(), 2u32, x);
+        dependency_provider.add_dependencies(i.to_string(), 1u32, []);
+    }
+
+    let name = "root".to_string();
+    let ver: u32 = 1;
+    let resolution = resolve(&dependency_provider, name, ver).unwrap();
+    let (p, _v) = resolution.into_iter().find(|(_p, v)| *v == 2).unwrap();
+    assert_eq!(p, "712".to_string());
 }
