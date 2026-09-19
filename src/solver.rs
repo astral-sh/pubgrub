08597ab9968ef8734c3cc0ba27a6295b8fe7322c
konstin <konstin@mailbox.org>
2026-09-19 23:59:56 +0200

    Keep obsolete incompatibility paths removed

--- src/solver.rs
+++ src/solver.rs
@@ -4,8 +4,8 @@ use std::collections::BTreeSet as Set;
 use std::error::Error;
 use std::fmt::{Debug, Display};
 
-use crate::internal::{Id, Incompatibility, State};
-use crate::{Map, Package, PubGrubError, Term, VersionSet};
+use crate::internal::{Id, State};
+use crate::{Map, Package, PubGrubError, VersionSet};
 use log::{debug, info};
 
 /// Tracks package versions whose dependency incompatibilities have already been added.
@@ -250,9 +250,8 @@ pub fn resolve<DP: DependencyProvider>(
         // Pick the next compatible version.
         let v = match decision {
             None => {
-                let inc =
-                    Incompatibility::no_versions(next, Term::Positive(term_intersection.clone()));
-                state.add_incompatibility(inc);
+                let versions = term_intersection.clone();
+                state.add_no_versions(next, versions);
                 continue;
             }
             Some(x) => x,
@@ -281,11 +280,7 @@ pub fn resolve<DP: DependencyProvider>(
 
             let dependencies = match dependencies {
                 Dependencies::Unavailable(reason) => {
-                    state.add_incompatibility(Incompatibility::custom_version(
-                        p,
-                        v.clone(),
-                        reason,
-                    ));
+                    state.add_unavailable(p, DP::VS::singleton(v), reason);
                     continue;
                 }
                 Dependencies::Available(x) => x,
